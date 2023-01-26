25d3c6bf3159f12d43ae0cae0582883a33a976c4
Tomas Fiers <tomas.fiers@gmail.com>
2023-01-26 10:41:21 +0000

    comment on changelog.md. but we're moving this to simple .jl script.bye :)

--- hello.js
+++ hello.js
@@ -15,3 +15,23 @@ const newLine = matchedLine.replace(currentVersion, versionToRelease)
 const newText = text.replace(matchedLine, newLine)
 
 fs.writeFileSync("test/out/Project.toml", newText)
+
+// Now, for Changelog.md
+// The whole new section to add can just be a hardcoded multiline string here.
+// Hm, also, now no need for commented out section anymore.
+
+/*
+<br>
+
+## v0.7  &nbsp;<sub>[![][unreleased-badge]][devlink]</sub>
+
+[unreleased-badge]: https://img.shields.io/badge/Unreleased-orange
+[devlink]: https://github.com/tfiers/PkgGraph.jl#development
+
+_{no changes yet}_
+*/
+
+// We could markdown-parse.
+// Or trust there'll be:
+// ## vXXXX  &nbsp;<sub>[![][unreleased-badge]][devlink]</sub>
+// ## vXXXX  &nbsp;<sub>[![][XXXX-date-badge]][XXXX-release]</sub>
